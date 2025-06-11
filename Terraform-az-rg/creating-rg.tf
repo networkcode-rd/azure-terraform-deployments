d118f383cab95ab1946b0dc7af13fe335bc38d0b
networkcode-rd <ratnavo.dutta.official@gmail.com>
2025-06-11 12:03:55 +0530

    working with Resource Group

--- Terraform-az-rg/creating-rg.tf
+++ Terraform-az-rg/creating-rg.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
     features {} # Configuration options
-    subscription_id = "bec0324e-4ff3-4e18-8d7b-2741bb57234f"
-    tenant_id       = "5bb19d03-7f41-4ea0-bce6-d62f8e15f1e6"
+    subscription_id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    tenant_id       = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
 
 resource "azurerm_resource_group" "new-tf-rg-2" {
