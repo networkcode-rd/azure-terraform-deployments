f396e0dc35e9d96fb9f5371d17d7a476d4761cbf
Ratnavo Dutta <networkcode-rd@gmail.com>
2025-07-03 11:56:50 +0530

    updating HCL basics

---
+++ HCL-basics/02-resource.tf
@@ -0,0 +1,4 @@
+resource "local_file" "networkcode" {
+  content  = "The whole content for this Github project, is to publish my work on Terraform.\n by \n Networkcode" 
+  filename = "${path.module}/networkcode.txt"
+}
