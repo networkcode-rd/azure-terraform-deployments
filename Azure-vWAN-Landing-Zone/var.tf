7f0a77a9cb0c80e0db0df47a28bdd9373d1aa8b7
Ratnavo Dutta <networkcode.rd@gmail.com>
2025-08-10 18:10:59 +0530

    Updating vWAN

---
+++ Azure-vWAN-Landing-Zone/var.tf
@@ -0,0 +1,65 @@
+variable "department_name" {
+  description = "Descibing the department name/ declared in .tfvars"
+  type        = string
+}
+
+variable "environment_name" {
+  description = "Descibing the environment name/ declared in .tfvars"
+  type        = string
+}
+
+variable "location_attribute" {
+  description = "Descibing the location attribute in naming convention"
+  type        = map(string)
+  default = {
+    "site1" = "CIN"
+    "site2" = "SEA"
+    "site3" = "EA"
+    "site4" = "WE"
+    "site5" = "EUS2"
+  }
+}
+
+variable "region_only_1" {
+  description = "describing region // declared in .tfvars"
+  type        = string
+}
+
+variable "region_only_2" {
+  description = "describing region // declared in .tfvars"
+  type        = string
+}
+
+variable "region_only_3" {
+  description = "describing region // declared in .tfvars"
+  type        = string
+}
+
+variable "region_only_4" {
+  description = "describing region // declared in .tfvars"
+  type        = string
+}
+
+variable "region_only_5" {
+  description = "describing region // declared in .tfvars"
+  type        = string
+}
+
+
+
+variable "vnet_information_linux" {
+  description = "passing the VNet value of Linux VM"
+  type = string
+  default = "/subscriptions/bec0324e-4ff3-4e18-8d7b-2741bb57234f/resourceGroups/eng-staging-CIN-tf-rg/providers/Microsoft.Network/virtualNetworks/eng-staging-CIN-vnet-tf"
+}
+
+variable "bep_ipadresses" {
+  description = "describing final value from modules"
+  type = string
+  default = "10.30.0.4"
+}
+
+# variable "lb_linux_private_ip_address" {
+#   description = "defining the private IP address of Azure Internal Load Balancer frontend IP configuration"
+#   type = string
+# }
